e91f899105633bad23d27fcd4ce25a25439b060d
Victor Fernando Faria Figueiredo <victor.figueiredo@sisema.net>
2025-06-30 16:44:37 -0300

    Testando pipeline CI Cypress com Allure

--- cypress/e2e/CT_26_verificarRolagemSemBotaoSeta.cy.js
+++ cypress/e2e/CT_26_verificarRolagemSemBotaoSeta.cy.js
@@ -5,6 +5,7 @@ describe('Verifica rolagem para baixo e para cima', () => {
   })
 
   it('Deve rolar a página para baixo e para cima sem utilizar o botão seta no canto inferior direito da página', () => {
+    // Teste
     cy.verificarRolagemSemBotao();
   })
 })
